0c729cdc52edd87b33401b30cf3b775c6794f5b7
Johan Sonesson <johan@traintech.eu>
2022-09-08 10:09:30 +0100

    helper class to make another class serializable

---
+++ src/BitDataParser/makeSerializable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BitDataParser
+{
+    [Serializable]
+    public abstract class makeSerializable
+    {
+        public void SerializeDataContract(string filePath, bool indent = true)
+        {
+            DataContractSerializer serializer = new DataContractSerializer(this.GetType(), new DataContractSerializerSettings()
+            { MaxItemsInObjectGraph = Int32.MaxValue });
+            using (var file = new FileStream(filePath, FileMode.Create))
+            {
+                var xmlwriter = XmlWriter.Create(file, new XmlWriterSettings() { Indent = indent });
+                serializer.WriteObject(xmlwriter, this);
+            }
+        }
+
+        public void SerializeXml(string filePath, bool indent = true)
+        {
+            XmlSerializer serializer = new XmlSerializer(this.GetType());
+            using (var file = new FileStream(filePath, FileMode.Create))
+            {
+                var xmlwriter = XmlWriter.Create(file, new XmlWriterSettings() { Indent = indent });
+                serializer.Serialize(xmlwriter, this);
+            }
+        }
+    }
+}
